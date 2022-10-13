a2208c693c690b754cf4d588760d0ec86cccbfac
Alice Zalambani <alice.zalambani@edu.unife.it>
2022-10-13 11:13:47 +0200

    Secondo esercizio

--- laboratorio3.c
+++ laboratorio3.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
-void divisori(int n){
-    printf ("Divisori: ");
-    for (int i=1; i<=n; i++){
+int isprimo(int n){
+    int c=0;
+    for (int i=2; i<=(n-1); i++){
         if (n%i==0)
+            c++;
+        
+    }
+    if (c==0)
+        return 1;
+    else
+        return 0;
+}
+
+void divisori_primi(int n){
+    printf ("Divisori: ");
+    for (int i=2; i<=n/2; i++){
+        if (n%i==0 && isprimo(i))
             printf ("%d ", i);
     }
     printf ("\n");
@@ -13,6 +26,6 @@ int main(){
     int n;
     printf ("Inserisci un numero: ");
     scanf ("%d", &n);
-    divisori(n);
+    divisori_primi(n);
     return 0;
 }
