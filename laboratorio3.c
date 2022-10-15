a7f0ad331d4bf686c59d4f7a0c9c9c26d9b2100f
Alice Zalambani <alice.zalambani@edu.unife.it>
2022-10-15 11:58:28 +0200

    Quarto esercizio

--- laboratorio3.c
+++ laboratorio3.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 
-int isprimo(int n){
+int is1MCD(int n, int m){
     int c=0;
-    for (int i=2; i<=(n-1); i++){
-        if (n%i==0)
+    for (int i=1; i<=n; i++){
+        if (n%i==0 && m%i==0)
             c++;
-        
     }
-    if (c==0)
+    if (c==1)
         return 1;
     else
         return 0;
 }
 
-void fattorizzazione(int n){
-    int i=2;
-    printf ("Fattorizzazione: ");
-    while (n!=1){
-        if (n%i==0 && isprimo (i)){
-            printf ("%d ", i);
-            n/=i;
+void terne_pitagoriche(int ipotenusa){
+    int a, b, c=0, m=1;
+    printf ("Terne piatagoriche:\n");
+    do{
+        m++;
+        for (int n=m-1; n<m; n++){
+            if (is1MCD(n,m)){
+                a=m*m-n*n;
+                b=2*m*n;
+                c=m*m+n*n;
+                if (c>ipotenusa)
+                    break; 
+                printf ("%d %d %d \n", a, b, c);
+            }
         }
-        else
-            i++;
-    }
-    printf ("\n");
+    }while (c<=ipotenusa);
 }
 
 int main(){
     int n;
-    printf ("Inserisci un numero: ");
+    printf ("Inserisci ipotenusa: ");
     scanf ("%d", &n);
-    fattorizzazione(n);
+    terne_pitagoriche(n);
     return 0;
 }
